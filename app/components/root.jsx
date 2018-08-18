2ae38d5be4b2fa5fb4dc75f1ba03705ed6b4d1e1
Adithya Krishna <adi2348492@gmail.com>
2018-08-18 18:01:08 +0530

    repositioned root element to point to Root instead of App

--- app/components/root.jsx
+++ app/components/root.jsx
@@ -1,19 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Provider } from 'react-redux';
 import './root.scss';
 
 import configureStore from 'store/configureStore';
 
-import App from 'app';
+import AppHeader from 'components/headers/appHeader';
+import AppSideBar from 'components/sideBars/appSideBars';
+import MainContent from 'components/mainContent/mainContent';
 
 /*----------  Configuring Store  ----------*/
 const store = configureStore();
 
 /*----------  Wrapping redux Provider ----------*/
-const Root = () => (
-	<Provider store={store}>
-		<App />
-	</Provider>
-);
+export default class Root extends Component {
+	render() {
+		return (
+			<Provider store={store}>
+				<Fragment>
+					<AppHeader />
 
-export default Root;
+					<AppSideBar />
+
+					<MainContent />
+				</Fragment>
+			</Provider>
+		);
+	}
+}
